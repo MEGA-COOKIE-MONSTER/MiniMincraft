4c51e7023327402dfb2b7575e7c364a67d3c0c4a
MEGA-COOKIE-MONSTER <120087497+MEGA-COOKIE-MONSTER@users.noreply.github.com>
2024-03-26 15:55:19 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -83,7 +83,7 @@ function draw(){
         
             
         }
-        if(frameCount% 15== 0 ){
+        if(frameCount% 10== 0 ){
             nukeyFun = createSprite(alex.x, alex.y)
             nukeyFun.addImage(NFImg)
             nukeyFun.scale = 0.125
